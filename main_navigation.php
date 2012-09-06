f4f28f47a64394bd271da64222e8512427075e16
James O'Neill <james@magicalnakama.com>
2012-09-06 13:32:40 +0100

    Navigation Permalink Change
    Changed the way permalinks function in the navigation by using the
    title rather than the ID number. Means links will still function if a
    new database is used as long as pages are created with the same title.

--- main_navigation.php
+++ main_navigation.php
@@ -13,8 +13,8 @@
 	
 	<ul id="navigation">
 		<li><a href="<?php bloginfo('url'); ?>">HOME</a></li>
-		<li><a href="<?php echo get_permalink( 11 ); ?>">ABOUT ME</a></li>
-		<li><a href="<?php echo get_permalink( 32 ); ?>">PORTFOLIO</a></li>
+		<li><a href="<?php echo get_permalink( get_page_by_title( 'About Me' ) ); ?>">ABOUT ME</a></li>
+		<li><a href="<?php echo get_permalink( get_page_by_title( 'Portfolio' ) ); ?>">PORTFOLIO</a></li>
 		<!--<li><a href="#">POST LISTING</a></li>-->
 	</ul>
 </div><!--#navigation-->
