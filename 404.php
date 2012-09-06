fc419f7c23a9706644259fb3e82bc64eb4cae5f0
James O'Neill <james@magicalnakama.com>
2012-09-06 12:35:34 +0100

    404.php
    Added content for 404.php and accompanying CSS rules.

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+include (TEMPLATEPATH . '/header.php');
+include (TEMPLATEPATH . '/home_header.php');
+?>
+
+<div id="four_o_four">
+	<h2>The Page You Requested Could Not Be Found</h2>
+	<p>Sorry but that page doesn't seem to exist. Please feel free to browse the rest of our content.</p>
+</div><!--#four_o_four-->
+
+
+<?php
+include (TEMPLATEPATH . '/primary_blogloop.php');
+include (TEMPLATEPATH . '/additional_content.php');
+include (TEMPLATEPATH . '/main_navigation.php');
+include (TEMPLATEPATH . '/footer.php');
+?>
